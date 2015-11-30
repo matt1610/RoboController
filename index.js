6513f793fed9fd1e4a1a551e0c9a4c35cc27c591
matt1610 <mattstarkey@me.com>
2015-11-30 22:23:26 +0200

    Port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cors());
 
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
